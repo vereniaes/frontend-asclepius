87070c0b87c59038579c5869fc7c24280cf9b66a
vereniaes <titasari_1519622036@mhs.unj.ac.id>
2024-12-04 01:23:37 +0700

    this is front-end app

---
+++ src/scripts/api.js
@@ -0,0 +1,20 @@
+// TODO: Silakan sesuaikan BASE URL dari endpoint Anda
+const BASE_URL =
+  "https://asclepius-backend-54347885804.asia-southeast2.run.app";
+
+const ENDPOINT = {
+  predict: `${BASE_URL}/predict`,
+};
+
+class PredictAPI {
+  static async predict(data) {
+    const response = await fetch(ENDPOINT.predict, {
+      method: "POST",
+      body: data,
+      redirect: "follow",
+    });
+
+    const json = await response.json();
+    return json;
+  }
+}
